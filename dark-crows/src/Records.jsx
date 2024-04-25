4aae96d7eec6710a9b847af69307a3795ad1e50b
youssef <youssefmmagdy55@gmail.com>
2024-04-25 15:14:17 +0200

    .

---
+++ dark-crows/src/Records.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Record from "./Record.jsx";
+import orgs from "./orgs.js";
+
+function Records(){
+    return (
+        <div className=" h-auto">
+            <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+      <Record name="name" description="desc"/>
+        </div>
+    );
+}
+
+export default Records;
