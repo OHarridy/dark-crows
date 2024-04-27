74491e514bb5fd185d837f680a513d515fac820d
Naefu <ahmoslamy@hotmail.com>
2024-04-28 00:13:33 +0300

    finished card prototype

--- dark-crows/src/mainPage.jsx
+++ dark-crows/src/mainPage.jsx
@@ -3,7 +3,6 @@ const Home = () => {
 
     return (
         <div className="home min-h-screen flex items-end mx-auto" style={{width:1400}}>
-
             <div className="flex flex-col">
                 <div className="h-[700px] w-[1000px]">
                 </div>
