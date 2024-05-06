a7534d4753fdb8d0d728a3bbb7df60dd287587d0
youssef <youssefmmagdy55@gmail.com>
2024-05-06 17:56:05 +0300

    na2es delete bas 8er kda kolo tmam

--- dark-crows/src/Admin/AdminRegisteredDonors.jsx
+++ dark-crows/src/Admin/AdminRegisteredDonors.jsx
@@ -4,29 +4,14 @@ import "./adminToggle.css";
 import donors from "./donors.jsx";
 import Search from "./Search.jsx";
 import AdminPicture from "./AdminPicture.jsx";
-
+import TheBAR from "../TheBAR.jsx";
+import AdminNavbar from "./AdminNavbar.jsx";
+import List from "./List.jsx";
 
 
 function AdminRegisteredDonors(){
     return (
-        <div className="flex flex-col justify-center items-center">
-            <div className="flex flex-row justify-between">
-                <AdminPicture />
-                <Search />
-                
-            </div>
-            <div className="text-4xl p-5">Donors</div>
-            <div className="flex flex-col">
-                <div className="flex flex-row">
-                    
-                    
-                </div>
-                <div style={{width:'70vw'}} className="flex-row flex flex-wrap justify-center">
-                    {donors.map(Card)}
-                </div>
-
-            </div>
-        </div>
+        <div className='flex flex-row mt-5 justify-center w-full'><div className="w-2/3"><List /></div> </div>
     );
 }
 
